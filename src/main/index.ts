dcb490ef87249ddad36ea8d18aaa5794d7597308
q/stol <hsgw@qstol.info>
2021-04-11 00:28:45 +0900

    custom scheme

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,10 @@
+import Router from "url-router";
 import { app } from "electron";
-import { onAppReady } from "./onReady";
+import isDev from "electron-is-dev";
+import { onAppReady } from "./app";
+import path from "path";
+
+console.log("HELLO");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
@@ -7,4 +12,36 @@ if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
+app.removeAsDefaultProtocolClient("bndlr");
+
+if (isDev && process.platform === "win32") {
+  console.log(path.resolve(process.execPath, process.argv[1]));
+  app.setAsDefaultProtocolClient("bndlr", process.execPath, [
+    path.resolve(process.argv[1]),
+  ]);
+} else {
+  app.setAsDefaultProtocolClient("bndlr");
+}
+
 app.on("ready", onAppReady);
+
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on("second-instance", (e, argv) => {
+    const rawUrl = argv.find((arg) => arg.startsWith("bndlr://"));
+    if (!rawUrl) return;
+    const url = rawUrl.replace(/^bndlr:\/\//, "");
+    type Handler = (params: Record<string, string>) => void;
+    const router = new Router<Handler>();
+
+    router.add("manifest/bms?url=:url(.*)", (params) => {
+      console.log("will load manifest", params);
+    });
+
+    const res = router.find(url);
+    res?.handler(res.params);
+  });
+}
